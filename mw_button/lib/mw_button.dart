504faa9c282dabdf6feca19b913f33b663264904
Mike <mike@lunasoft.co.za>
2022-11-10 15:36:56 +0200

    Fixes
    date_selector: Included necessary libraries
    button: corrected param
    raised_button: Corrected param

--- mw_button/lib/mw_button.dart
+++ mw_button/lib/mw_button.dart
@@ -7,14 +7,14 @@ import 'package:flutter/widgets.dart';
 class MWButton extends StatelessWidget {
 
 Function? onPressed;
-required String text;
+String text;
 Color textColor = Colors.white;
 Color? color;
 IconData? iconData;
 EdgeInsets? margin;
 bool disabled = false;
 
-MWButton({this.onPressed, this.text, this.textColor = Colors.white, this.color, this.iconData, this.margin, this.disabled = false});
+MWButton({this.onPressed, required this.text, this.textColor = Colors.white, this.color, this.iconData, this.margin, this.disabled = false});
 
   @override
   Widget build(BuildContext context) {
