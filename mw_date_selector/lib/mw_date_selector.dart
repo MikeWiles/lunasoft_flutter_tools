cfd5e8fec5c4218f6baa455bc0e5f13a065af905
Michael Wiles <Mike18@mailinator.com>
2023-11-02 10:45:35 +0200

    Feature (date selector): Update colouring parameters

--- mw_date_selector/lib/mw_date_selector.dart
+++ mw_date_selector/lib/mw_date_selector.dart
@@ -10,12 +10,16 @@ class MWDateSelector extends StatefulWidget {
   DateTime? minTime;
   DateTime? maxTime;
   Function(DateTime) onDateChange;
+  Color? color;
+  Color? iconColor;
+  Color? emptyColor;
+  Color? textColor;
   DateTime? _date;
 
-  MWDateSelector({Key? key, required this.label, required this.onDateChange, this.minTime, this.maxTime}) : super(key: key);
+  MWDateSelector({Key? key, required this.label, required this.onDateChange, this.minTime, this.maxTime, this.color, this.emptyColor, this.iconColor, this.textColor}) : super(key: key);
 
   @override
-  _MWDateSelectorViewState createState() => _MWDateSelectorViewState(this.label, this.onDateChange, this.minTime, this.maxTime);
+  _MWDateSelectorViewState createState() => _MWDateSelectorViewState(this.label, this.onDateChange, this.minTime, this.maxTime, this.color, this.emptyColor, this.iconColor, this.textColor);
 }
 
 class _MWDateSelectorViewState extends State<MWDateSelector> {
@@ -23,12 +27,16 @@ class _MWDateSelectorViewState extends State<MWDateSelector> {
   String label;
   DateTime? minTime;
   DateTime? maxTime;
+  Color? color;
+  Color? iconColor;
+  Color? emptyColor;
+  Color? textColor;
   Function(DateTime) onDateChange;
   DateTime? _date;
 
   DateFormat dateFormat = DateFormat("dd MMM yyyy");
 
-  _MWDateSelectorViewState(this.label, this.onDateChange, this.minTime, this.maxTime);
+  _MWDateSelectorViewState(this.label, this.onDateChange, this.minTime, this.maxTime, this.color, this.emptyColor, this.iconColor, this.textColor);
 
   @override
   void initState() {
@@ -36,10 +44,10 @@ class _MWDateSelectorViewState extends State<MWDateSelector> {
   }
 
   _getColour() {
-    if (_date == null) {
-      return Colors.black45;
+    if (_date == null && emptyColor != null) {
+      return emptyColor;
     }
-    return Theme.of(context).primaryColor;
+    return color ?? Theme.of(context).primaryColor;
   }
 
   @override
@@ -48,10 +56,10 @@ class _MWDateSelectorViewState extends State<MWDateSelector> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(label, style: const TextStyle(
+          Text(label, style: TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w400,
-              color: Colors.black54)),
+              color: textColor ?? _getColour())),
           Container(
               margin: const EdgeInsets.only(top: 4),
               padding: const EdgeInsets.all(2),
@@ -72,29 +80,29 @@ class _MWDateSelectorViewState extends State<MWDateSelector> {
                         );
                       },
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children:[
-                                Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child:Icon(Icons.date_range, color: _date == null ? Colors.black45 : Theme.of(context).primaryColor)
-                                ),
-                                Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child: Text(_date != null ? dateFormat.format(_date!) : "Select Date", style:TextStyle(color: _getColour(), fontWeight: FontWeight.w600, fontSize: 14))
-                                )
-                              ]
-                          ),
-                          _date != null ? GestureDetector(
-                            onTap: () => setState(() => _date = null),
-                            child: Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: Icon(Icons.close, color: Theme.of(context).colorScheme.error)
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children:[
+                                  Padding(
+                                      padding: const EdgeInsets.all(4),
+                                      child:Icon(Icons.date_range, color: iconColor ?? _getColour())
+                                  ),
+                                  Padding(
+                                      padding: const EdgeInsets.all(4),
+                                      child: Text(_date != null ? dateFormat.format(_date!) : "Select Date", style:TextStyle(color: _getColour(), fontWeight: FontWeight.w600, fontSize: 14))
+                                  )
+                                ]
                             ),
-                          ): Container()
-                        ]
+                            _date != null ? GestureDetector(
+                              onTap: () => setState(() => _date = null),
+                              child: Padding(
+                                  padding: const EdgeInsets.all(4),
+                                  child: Icon(Icons.close, color: Theme.of(context).colorScheme.error)
+                              ),
+                            ): Container()
+                          ]
                       )
                   ),
                   color: Colors.transparent
